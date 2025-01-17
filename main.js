08ae92b5882e0c6a417f9511904872fcd22f8b4a
SarJHunt <184242720+SarJHunt@users.noreply.github.com>
2025-01-17 10:10:59 +0000

    Update main.js

--- main.js
+++ main.js
@@ -9,18 +9,6 @@
 
 export function countryTotalWins(worldCups, euros, country) {
     //Your code here...
-     let totalWins = 0;
-        
-    //Normalize the country name to lowercase
-        const normalisedCountry = country.toLowerCase();
-      
-    // Count World Cup wins (case-insensitive)
-        totalWins += worldCups.filter(match => match.winner.toLowerCase() === normalisedCountry).length;
-        
-    // Count Euro wins (case-insensitive)
-        totalWins += euros.filter(match => match.winner.toLowerCase() === normalisedCountry).length;
-      
-        return totalWins;
 }
 
 // */
